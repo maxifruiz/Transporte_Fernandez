e37efe0dd855f3dcbe52c39321d0b6b6d5b425c2
maxifruiz <maxif.ruiz@gmail.com>
2025-06-05 18:17:45 -0300

    Tu mensaje

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,6 @@ import { Autoplay, EffectFade } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/autoplay';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, EffectFade } from 'swiper/modules';
 
 const Hero = () => {
   const images = [
